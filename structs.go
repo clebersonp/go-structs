99b7c0f88150a7601815cd9359b073fd7c6f658f
Cleberson <pauluci.cleberson@gmail.com>
2024-02-12 17:45:02 -0300

    Refactored all code to use the package user

--- structs.go
+++ structs.go
@@ -1,45 +1,9 @@
 package main
 
 import (
-	"errors"
+	"example.com/structs/user"
 	"fmt"
-	"time"
 )
-
-type user struct {
-	firstName string
-	lastName  string
-	birthDate string
-	createdAt time.Time
-}
-
-// in Go we don't create methods inside struct, but we ref the method to struct by using a 'Receiver'.
-// Receiver must be after func keyword but before the method name.
-// We link these two pieces together by a 'Receiver'
-func (u *user) outputUserDetails() {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-}
-
-// in Go every thing is passed as copy, so in this case we need to use a pointer to guaranty that the same object pointer is passed here
-func (u *user) clearUserFullName() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-// this is a utility method, a convention or pattern it's not a Go feature to creation structs
-func newUser(firstName, lastName, birthdate string) (*user, error) {
-	// adding validation for utility struct constructor
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("first name, last name and birthdate are required")
-	}
-
-	return &user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthdate,
-		createdAt: time.Now(),
-	}, nil
-}
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
@@ -48,7 +12,8 @@ func main() {
 
 	// Struct literal or composite literal (instantiate a struct)
 	// pointer of user struct
-	var appUser *user
+	// the first lower case user is the package name and the last one is struct
+	var appUser *user.User
 
 	//appUser = user{} // user with all fields with 'null value' (default values)
 	// short notation without specify the field names
@@ -59,7 +24,7 @@ func main() {
 	//	time.Now(),
 	//}
 
-	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+	appUser, err := user.NewUser(userFirstName, userLastName, userBirthdate)
 
 	// if the args are not valid then return and stop the application
 	if err != nil {
@@ -68,9 +33,9 @@ func main() {
 	}
 
 	// this is a user method
-	appUser.outputUserDetails()
-	appUser.clearUserFullName()
-	appUser.outputUserDetails()
+	appUser.OutputUserDetails()
+	appUser.ClearUserFullName()
+	appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {

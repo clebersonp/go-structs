93057a698fb853e71c897d28ce3a4c14259dc365
Cleberson <pauluci.cleberson@gmail.com>
2024-02-12 17:32:17 -0300

    Added validation to utility struct constructor

--- structs.go
+++ structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -26,13 +27,18 @@ func (u *user) clearUserFullName() {
 }
 
 // this is a utility method, a convention or pattern it's not a Go feature to creation structs
-func newUser(firstName, lastName, birthdate string) *user {
+func newUser(firstName, lastName, birthdate string) (*user, error) {
+	// adding validation for utility struct constructor
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("first name, last name and birthdate are required")
+	}
+
 	return &user{
 		firstName: firstName,
 		lastName:  lastName,
 		birthDate: birthdate,
 		createdAt: time.Now(),
-	}
+	}, nil
 }
 
 func main() {
@@ -53,7 +59,13 @@ func main() {
 	//	time.Now(),
 	//}
 
-	appUser = newUser(userFirstName, userLastName, userBirthdate)
+	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+
+	// if the args are not valid then return and stop the application
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	// this is a user method
 	appUser.outputUserDetails()
@@ -64,6 +76,6 @@ func main() {
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
